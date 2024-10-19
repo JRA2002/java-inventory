239c2ab78dbef7136e432e250dc6c53e8c8abbab
javier <javier_2790@outlook.com>
2024-10-19 21:35:14 +0200

    finish with hashing passwords

--- src/main/java/com/inventorymanagementsystem/LoginController.java
+++ src/main/java/com/inventorymanagementsystem/LoginController.java
@@ -52,6 +52,7 @@ public class LoginController implements Initializable {
 
 
     public void textfieldDesign(){
+
         if(username.isFocused()){
             username.setStyle("-fx-background-color:#fff;"+"-fx-border-width:2px");
             password.setStyle("-fx-background-color:transparent;"+"-fx-border-width:1px");
@@ -67,55 +68,83 @@ public class LoginController implements Initializable {
     public void onExit(){
         System.exit(0);
     }
-
-
-    public void login(){
-
-        connection= Database.getInstance().connectDB();
-        String sql="SELECT * FROM users WHERE username=? and password=?";
-        try{
-            preparedStatement=connection.prepareStatement(sql);
-            preparedStatement.setString(1,username.getText());
-            preparedStatement.setString(2, password.getText());
-            resultSet=preparedStatement.executeQuery();
-            if(resultSet.next()){
-
-                int userId = resultSet.getInt("id");
-                String userName = resultSet.getString("username");
-                String userRol = resultSet.getString("rol");
-                User userData = new User(userId, userName, userRol);
-                Session.setCurrentUser(userData);
-
-                Alert alert=new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Mensaje de Bienvenida");
+    private String getHashedPass(){
+        String hashedPass = "";
+        Connection connection = Database.getInstance().connectDB();
+        String sql = "SELECT password FROM users WHERE username=?";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, username.getText());
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                hashedPass = resultSet.getString("password");
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Message");
                 alert.setHeaderText(null);
-                alert.setContentText("Datos Correctos !");
-                alert.showAndWait();
-                login_btn.getScene().getWindow().hide();
-                Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
-                Scene scene = new Scene(root);
-                Stage stage=new Stage();
-
-                root.setOnMousePressed((event)->{
-                    x=event.getSceneX();
-                    y=event.getSceneY();
-                });
-                root.setOnMouseDragged((event)->{
-                    stage.setX(event.getScreenX()-x);
-                    stage.setY(event.getScreenY()-y);
-                });
-                stage.initStyle(StageStyle.TRANSPARENT);
-                stage.setScene(scene);
-                stage.show();
-            }else{
-                Alert alert=new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Mensaje de Error");
-                alert.setHeaderText(null);
-                alert.setContentText("Username o Password Incorrectos");
+                alert.setContentText("USUARIO NO ENCONTRADO.");
                 alert.showAndWait();
             }
-        }catch (Exception err){
+        } catch (Exception err) {
             err.printStackTrace();
+        }
+        return hashedPass;
+    }
+
+    public boolean isCorrectPassword(){
+        String hashedPass = getHashedPass();
+        String enterPass = password.getText();
+        return  Password.check(enterPass,hashedPass).withBcrypt();
+    }
+
+    public void login(){
+        String hashedPass = getHashedPass();
+        if(isCorrectPassword()){
+            connection= Database.getInstance().connectDB();
+            String sql="SELECT * FROM users WHERE username=? and password=?";
+            try{
+                preparedStatement=connection.prepareStatement(sql);
+                preparedStatement.setString(1,username.getText());
+                preparedStatement.setString(2, hashedPass);
+                resultSet=preparedStatement.executeQuery();
+                if(resultSet.next()){
+                    int userId = resultSet.getInt("id");
+                    String userName = resultSet.getString("username");
+                    String userRol = resultSet.getString("rol");
+                    User userData = new User(userId, userName, userRol);
+                    Session.setCurrentUser(userData);
+
+                    Alert alert=new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Mensaje de Bienvenida");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Datos Correctos !");
+                    alert.showAndWait();
+                    login_btn.getScene().getWindow().hide();
+                    Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+                    Scene scene = new Scene(root);
+                    Stage stage=new Stage();
+
+                    root.setOnMousePressed((event)->{
+                        x=event.getSceneX();
+                        y=event.getSceneY();
+                    });
+                    root.setOnMouseDragged((event)->{
+                        stage.setX(event.getScreenX()-x);
+                        stage.setY(event.getScreenY()-y);
+                    });
+                    stage.initStyle(StageStyle.TRANSPARENT);
+                    stage.setScene(scene);
+                    stage.show();
+                }
+            }catch (Exception err){
+                err.printStackTrace();
+            }
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("CONTRASEÑA INCORRECTA !!.");
+            alert.showAndWait();
         }
     }
 
